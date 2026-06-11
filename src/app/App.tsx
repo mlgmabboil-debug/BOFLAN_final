749f62ad748c3d1295325b9fed279b7a0e45717d
mlgmabboil-debug <mlgmabboil@gmail.com>
2026-06-12 00:58:14 +0300

    щцщвщцв

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,10 @@ import { lazy, Suspense } from "react";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { UserProvider, useUser } from "./context/UserContext";
+import { Web3ModalProvider } from "./components/Web3Modal";
 
 const Onboarding = lazy(() =>
   import("./pages/Onboarding").then((mod) => ({ default: mod.Onboarding }))
-);
-const Web3ModalProvider = lazy(() =>
-  import("./components/Web3Modal").then((mod) => ({ default: mod.Web3ModalProvider }))
 );
 
 function AppInner() {
@@ -23,11 +21,9 @@ function AppInner() {
   }
 
   return (
-    <Suspense fallback={<div className="min-h-screen bg-[#000000]" />}>
-      <Web3ModalProvider>
-        <RouterProvider router={router} />
-      </Web3ModalProvider>
-    </Suspense>
+    <Web3ModalProvider>
+      <RouterProvider router={router} />
+    </Web3ModalProvider>
   );
 }
 
